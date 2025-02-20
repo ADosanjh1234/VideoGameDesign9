0f47d1b9b6df1040803981ff1a9e2f1e8b78cf4d
ADosanjh1234 <ashvind122@stemcollegiate.ca>
2025-02-20 10:34:10 -0700

    Create Feb_20.cs

---
+++ Feb_20.cs
@@ -0,0 +1,10 @@
+string firstName; 
+firstName = "Ashvin"; 
+Console.WriteLine(firstName); 
+firstName = "Liem";
+Console.WriteLine(firstName); 
+firstName = "Isabella"; 
+Console.WriteLine(firstName); 
+firstName = "Yasmine"; 
+Console.WriteLine(firstName); 
+//I learned how to set and get values from variables. 
