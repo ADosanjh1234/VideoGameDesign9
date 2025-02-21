b130696eecfb762048164dd76495095493715692
ADosanjh1234 <ashvind122@stemcollegiate.ca>
2025-02-21 09:01:31 -0700

    Create Feb_21.cs

---
+++ Feb_21.cs
@@ -0,0 +1,11 @@
+//Lesson 5 
+var message = "Hi world";
+var message = "1000"
+  //There was an error. 
+// Lesson 6 
+  string firstName = "amanda"; 
+int number = 350;
+double temp = 20.6;
+Console.WriteLine($" hi, {firstName}! You have {number} messages in your inbox. The temprature is {temp} celciuis."); 
+// It worked and the message was  "hi, amanda! You have 350 messages in your inbox. The temprature is 20.6 celciuis." 
+// I learned how to make a statement with coding. 
