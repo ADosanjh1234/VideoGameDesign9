a64a4a8a48c13afaf72ffeaebae0123171f352f8
ADosanjh1234 <ashvind122@stemcollegiate.ca>
2025-05-01 10:35:39 -0600

    Create May1.cs

---
+++ May1.cs
@@ -0,0 +1,9 @@
+int sophiaSum = 0;
+int nicolasSum = 0;
+int zahirahSum = 0;
+int jeongSum = 0;
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahirah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
+// we learned to calculate numbers and put them infront of names
